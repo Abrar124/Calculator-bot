29553a4af8a0f6cb527d9d1c0a90b4ded6a2c117
Abrar124 <abrar.khurshid.124@gmail.com>
2019-02-07 14:37:46 +0500

    Complete calculator bot with firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,49 @@
 const functions = require('firebase-functions');
+var admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+var firestore = admin.firestore();
 
 exports.webhook = functions.https.onRequest((request, response) => {
-    
-        console.log("request.body.result.parameters: ", request.body.result.parameters);
-        let params = request.body.result.parameters;
-    var x= params.number;
-    console.log(x);
-        response.send({
-            speech:
-                `Your results is ${parseInt(params.number)  + parseInt(params.number2) }`
-        });
-    });
+    let params = request.body.result.parameters;
 
-    //  function Subtraction (agent) {
-    //     agent.add(`I didn't understand`);
-    //     agent.add(`I'm sorry, can you try again?`);
-    //   }
-    //   exports.Subtraction = functions.https.onRequest((request, response) => {
 
-    //     response.send("I'm sorry, can you try again?");
-    
-    // });
+    switch (request.body.result.action) {
+
+        case 'addition':
+            
+            response.send({
+                speech:
+                    `Your results is ${parseInt(params.number) + parseInt(params.number2)}`
+            });
+            break;
+
+        case 'subtraction':
+
+            response.send({
+                speech:
+                    `Your results is ${parseInt(params.number) - parseInt(params.number2)}`
+            });
+            break;
+
+            case 'Multiply':
+
+            response.send({
+                speech:
+                    `Your results is ${parseInt(params.number) * parseInt(params.number2)}`
+            });
+            break;
+
+            case 'Divide':
+
+            response.send({
+                speech:
+                    `Your results is ${parseInt(params.number) / parseInt(params.number2)}`
+            });
+            break;
+
+        default:
+            response.send({
+                speech: "no action matched in webhook"
+            })
+    }
+});
